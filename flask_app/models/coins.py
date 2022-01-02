6d811153c480b71090c44dd83b037d23473cbeef
Claude Will <claude.will@yahoo.com>
2022-01-02 00:04:55 -0600

    work done on coins model

--- flask_app/models/coins.py
+++ flask_app/models/coins.py
@@ -1,4 +1,4 @@
-from flask_app.config.mysqlconnection import connectToMySql
+from CoinCollectingApp.flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 import re
 
@@ -14,5 +14,21 @@ class Coin:
         self.grade = data['grade']
         self.initialCost = data['initialCost']
         self.value = data['value']
+        self.user_id = data['user_id']
         self.added_at = data['added_at']
         self.updated_at = data['updated_at']
+        
+    @classmethod
+    def createCoin(cls,data):
+        q = "INSERT INTO coins (date,mintMark,denomination,gradingService,certificationNum,grade,initialCost,value, user_id) VALUES (%(date)s,%(mintMark)s,%(denomination)s,%(gradingService)s,%(certificationNum)s,%(grade)s,%(initialCost)s,%(value)s,%(user_id)s);"
+        return connectToMySQL(cls.db).query_db(q,data)
+    
+    @classmethod
+    def getAllCoins(cls):
+        q = 'SELECT * FROM coins AS c LEFT JOIN users AS u ON u.id = c.user_id;'
+        results = connectToMySQL(cls.db).query_db(q)
+        all_coins = []
+        if not results:
+            return all_coins
+        for coin in results:
+            new_coin = cls(coin)
